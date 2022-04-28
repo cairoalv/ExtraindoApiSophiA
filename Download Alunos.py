8f1722302d9fc2d07649ae051de1bd499cb3602a
Cairo Alves <cairo.alvcosta@gmail.com>
2022-04-28 15:37:29 -0300

    Att

--- Download Alunos.py
+++ Download Alunos.py
@@ -1,4 +1,7 @@
+import json
 import requests
+import pandas as pd
+import openpyxl
 
 
 chav2 = ''
@@ -13,16 +16,20 @@ headers = {
 
 url = requests.get("https://portal.sophia.com.br/SophiAWebAPI/3055/api/v1/Alunos", headers=headers).json()
 
-response_dados = url
-
 CodAluno = []
 NomeAluno = []
 EmailAluno = []
 TurmaAluno = []
 
+
 for aluno in url:
     CodAluno.append(aluno['codigoExterno'])
     NomeAluno.append(aluno['nome'])
     EmailAluno.append(aluno['email'])
+    TurmaAluno.append(aluno['turmas'])
 
+print(TurmaAluno)
 
+dados = pd.DataFrame(list(zip(CodAluno, NomeAluno, EmailAluno)), columns=['CodigoExterno','Nome','Email'])
+
+dados.to_excel('Alunos.xlsx', index = False)
